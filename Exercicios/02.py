efde944dde257ea2cc93ce330c260f7cc173a014
Elçom <elcomj31@gmail.com>
2022-05-30 20:48:58 -0300

    Exercicios 01 e 02

---
+++ Exercicios/02.py
@@ -0,0 +1,4 @@
+#Desafio 002 - Digitar o seu nome e o sistema ira lhe retornar dando uma mensagem de boas vindas
+
+nome = input('Digite o seu nome: ')
+print('É um prazer te conhecer, {}!'.format(nome))  #Usa o {} para fazer uma saida formatada com o .format(), ou seja, ele irá formatadar a variavel dentro do () para sair no {}
