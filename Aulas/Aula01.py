19f2957a50d56eebd4586dcdd85635aa780bb9da
Elçom <elcomj31@gmail.com>
2022-05-31 13:13:13 -0300

    Criaçao de Pastas

---
+++ Aulas/Aula01.py
@@ -0,0 +1,13 @@
+# Aqui eu começo meus estudos em python
+# Sempre quando acabar o código quebrar uma linha para o compilador entender que o código acaba ali
+# Dica - Entrar Zen mode (Crtl + K Z) e para sair (Crtl K Z)
+
+print ('Hello World')
+print ('Diga oi para todos')
+print ('Meu nome é Elçom')
+
+print ('Elcom' + 'Junior') #O + só pode ser usado quando for para mostrar duas strings juntas.
+print (7+5) 
+
+#print ('Olá' + 5) - Ao usar o + não é possível concatenar string com número
+print ('Olá', 5)
