c013e5ccc39998cf4ac254f3aa381672dc87857d
Elçom <elcomj31@gmail.com>
2022-05-31 10:32:31 -0300

    Método .is

---
+++ Exercicios/03.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia dois numeros e mostra a soma entre eles
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1+n2
+
+print('A soma entre {} e {} é de {}'.format(n1, n2, soma))
---
+++ Exercicios/04.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ela.
+
+a1 = input('Digite algo: ')
+a2 = input('Digite algo: ')
+a3 = input('Digite algo: ')
+a4 = input('Digite algo: ')
+
+print(a1.isnumeric())        # Verifica se o valor digitado é númerico e retorna True or False
+print(a2.isalpha())          # Verifica se o valor digitado é alfabético e retorna True or False
+print(a3.isalnum())          # Verifica se o valor digitado é alfa-numérico e retorna True or False
+print(a4.isupper())          # Verifica se o valor digitado é UPPERCASE e retorna True or False
