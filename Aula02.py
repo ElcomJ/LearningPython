818da1bfd869fbdb3a65fff7e106ecc80cb96a3b
Elçom <elcomj31@gmail.com>
2022-05-31 13:11:14 -0300

    Organização de Pastas criada

--- Aula02.py
+++
@@ -1,9 +0,0 @@
-#Toda variável é um Objeto em python
-# nome = ('Elcom')                   - Ja pré define o valor do objeto
-# nome = input('Qual é o seu nome?') - Recebe o valor do objeto por digitação
-
-nome  = input('Qual é o seu nome?')
-idade = input('Qual é a sua idade')
-sexo  = input('Qual é o seu sexo?')
-
-print (nome, idade, sexo) #A virgula é utilizada quando precisa se juntar uma string com uma váriavel numérica.
