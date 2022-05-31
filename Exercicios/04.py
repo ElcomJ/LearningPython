f2def078feda90718e101ab8222bf3ad10ea886e
Elçom <elcomj31@gmail.com>
2022-05-31 13:08:18 -0300

    Exercicios

--- Exercicios/04.py
+++ Exercicios/04.py
@@ -9,3 +9,8 @@ print(a1.isnumeric())        # Verifica se o valor digitado é númerico e retor
 print(a2.isalpha())          # Verifica se o valor digitado é alfabético e retorna True or False
 print(a3.isalnum())          # Verifica se o valor digitado é alfa-numérico e retorna True or False
 print(a4.isupper())          # Verifica se o valor digitado é UPPERCASE e retorna True or False
+
+print('{}'.format(type(a1)))
+print('{}'.format(type(a2)))
+print('{}'.format(type(a3)))
+print('{}'.format(type(a4)))
---
+++ Pratica/Listas.py
@@ -0,0 +1,5 @@
+lista1 = ['python', 'php', 'javascript']
+lista2 = [22, 315.25, 10 + 5j]
+
+print(lista1[0])
+print(lista2[1])
