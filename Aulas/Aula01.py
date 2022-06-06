b5c21641195f5eb76c64df3d9e2a7709b543fa61
Elçom <elcomj31@gmail.com>
2022-06-06 17:55:18 -0300

    Correções

--- Aulas/Aula01.py
+++ Aulas/Aula01.py
@@ -2,12 +2,12 @@
 # Sempre quando acabar o código quebrar uma linha para o compilador entender que o código acaba ali
 # Dica - Entrar Zen mode (Crtl + K Z) e para sair (Crtl K Z)
 
-print ('Hello World')
-print ('Diga oi para todos')
-print ('Meu nome é Elçom')
+print('Hello World')
+print('Diga oi para todos')
+print('Meu nome é Elçom')
 
-print ('Elcom' + 'Junior') #O + só pode ser usado quando for para mostrar duas strings juntas.
-print (7+5) 
+print('Elcom' + 'Junior')  # O + só pode ser usado quando for para mostrar duas strings juntas.
+print(7 + 5)
 
-#print ('Olá' + 5) - Ao usar o + não é possível concatenar string com número
-print ('Olá', 5)
+# print ('Olá' + 5) - Ao usar o + não é possível concatenar string com número
+print('Olá', 5)
