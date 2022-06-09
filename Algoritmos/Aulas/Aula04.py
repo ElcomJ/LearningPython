7c8016211a81349f537b88cd9599a0a6dfe5ce1b
Elçom <elcomj31@gmail.com>
2022-06-09 07:46:06 -0300

    Organização do código.

--- Algoritmos/Aulas/Aula04.py
+++ Algoritmos/Aulas/Aula04.py
@@ -1,14 +1,14 @@
-#Tipos primitivos e saidas de dados
+# Tipos primitivos e saidas de dados
 
-#int   = número inteiro        | Ex: 1, 5, -4,884, 7, 0.
-#float = número real           | Ex: 4.5, 0.075, -15.225, 3.14, 7.0.
-#bool  = Valor verdadeiro      | Ex: True, False.
-#str   = Cadeia de caracteres  | Ex: 'Olá', '7.5', ''.
+# int   = número inteiro        | Ex: 1, 5, -4,884, 7, 0.
+# float = número real           | Ex: 4.5, 0.075, -15.225, 3.14, 7.0.
+# bool  = Valor verdadeiro      | Ex: True, False.
+# str   = Cadeia de caracteres  | Ex: 'Olá', '7.5', ''.
 
-n1=int(input('Digite um número: '))
-n2=int(input('Digite outro numero: '))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro numero: '))
 
-soma=n1+n2
+soma = n1 + n2
 
 # print('A soma de ',n1,' e de ',n2,' é de: {}'.format(soma))
-print('A soma entre {} e {} vale {}'.format(n1, n2, soma)) # A {} é utilizada para poder receber valores formatados com o .format
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))  # A {} é utilizada para poder receber valores formatados com o .format
