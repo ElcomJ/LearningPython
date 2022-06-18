a88360558ea28d7c382a8426f2c38691b7a86813
¨Elçom <elcomj31@gmail.com>
2022-06-17 22:34:25 -0300

    Python no NeoVim

--- EstruturaDeDados/teste.py
+++ EstruturaDeDados/teste.py
@@ -1,1 +1,8 @@
-print('Hello World')
+i = int(input("Digite um valor: "))
+j = int(input("Digite outro valor: "))
+
+soma = i + j
+divisao = i / j
+
+print("A soma de {} + {} é de {}".format(i, j, soma))
+print("A divisao de {} e {} é de {}".format(i, j, divisao))
