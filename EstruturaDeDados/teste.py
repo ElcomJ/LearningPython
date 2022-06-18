e0ab25631c01fd136524880e912b60d3f432bddb
¨Elçom <elcomj31@gmail.com>
2022-06-17 23:20:37 -0300

    Python no NeoVim

--- EstruturaDeDados/teste.py
+++ EstruturaDeDados/teste.py
@@ -1,8 +1,11 @@
 i = int(input("Digite um valor: "))
 j = int(input("Digite outro valor: "))
 
-soma = i + j
-divisao = i / j
+soma            = i + j
+divisao         = i / j
+multiplicação   = i * j
+
 
 print("A soma de {} + {} é de {}".format(i, j, soma))
 print("A divisao de {} e {} é de {}".format(i, j, divisao))
+print('A multiplicação de {} e {} é de {}'.format(i, j, multiplicação))
