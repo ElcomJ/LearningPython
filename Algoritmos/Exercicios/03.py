cd980fd9d350fde898717bd9f30a11f894b1166f
¨Elçom <elcomj31@gmail.com>
2022-06-20 18:13:50 -0300

    Python Neovim

--- Algoritmos/Exercicios/03.py
+++ Algoritmos/Exercicios/03.py
@@ -2,6 +2,9 @@
 
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
-soma = n1+n2
+n3 = int(input('Digite um numero: '))
+
+soma = n1+n2+n3
 
 print('A soma entre {} e {} é de {}'.format(n1, n2, soma))
+print('')
---
+++ EstruturaDeDados/Teste1.py
@@ -0,0 +1,14 @@
+i = int(input("Digite um valor: "))
+j = int(input("Digite outro valor: "))
+
+soma = i + j
+divisao = i / j
+multiplicação = i * j
+subtração = i - j
+
+print("\n")
+
+print(f"A soma de {i} e {j} é de {soma}")
+print(f"A divisao de {i} e {j} é de {divisao}")
+print(f"A multiplicação de {i} e {j} é de {multiplicação}")
+print(f"A subtração de {i} e {j} é de {subtração}")
