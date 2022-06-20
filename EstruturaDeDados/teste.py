1d5a040a7632b86e11238dd23e8df7865e9b7fc0
¨Elçom <elcomj31@gmail.com>
2022-06-20 08:18:07 -0300

    Python no NeoVim

--- EstruturaDeDados/teste.py
+++ EstruturaDeDados/teste.py
@@ -12,8 +12,3 @@ print("A soma de {} + {} é de {}".format(i, j, soma))
 print("A divisao de {} e {} é de {}".format(i, j, divisao))
 print('A multiplicação de {} e {} é de {}'.format(i, j, multiplicação))
 print('A subtracao de {} e {} é de {}'.format(i, j, subtracao))
-
-
-
-
-
