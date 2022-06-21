f42d02b89783b671d02ae29be27c1fa502be1d67
¨Elçom <elcomj31@gmail.com>
2022-06-21 13:06:40 -0300

    Matrizes.py

--- EstruturaDeDados/Matrizes.py
+++ EstruturaDeDados/Matrizes.py
@@ -1,4 +1,9 @@
-lista1 = ['python', 'java', 'php']
-
-
-print(lista1[1]
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l] [c] = int(input(f'Digite um valor para [{l}], [{c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l] [c]:^5}]', end='')
+    print()
